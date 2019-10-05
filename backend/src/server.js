bb2e500403b4a29b1b9b5f690f6d1a457a961808
Mathews Motta <mathewscsmotta@gmail.com>
2019-10-05 17:19:18 +0100

    Primeiras versões

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users',(req, res) => {
+    return res.json(req.body);
+});
+
+app.listen(3333);
