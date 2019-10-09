295e11cc338d6471a7d418388061fb21890bd206
Mathews Motta <mathewscsmotta@gmail.com>
2019-10-09 15:32:21 +0100

    Front end terminado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 
 const routes = require('./routes.js');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://persaircnc:rox123@pers-aircnc-q6hoe.mongodb.net/casoestudo?retryWrites=true&w=majority', {
+mongoose.connect('', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
 
